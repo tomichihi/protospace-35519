8e4b0599242b535762b710550ecd4757f457693c
tomichihi <tsshc820@gmail.com>
2021-05-11 15:35:45 +0900

    test2

--- db/schema.rb
+++ db/schema.rb
@@ -56,15 +56,15 @@ ActiveRecord::Schema.define(version: 2021_05_10_073831) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "name", default: "", null: false
-    t.string "profile", default: "", null: false
-    t.string "occupation", default: "", null: false
+    t.string "name", null: false
+    t.text "profile", null: false
+    t.text "position", null: false
+    t.text "occupation", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.text "position"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
